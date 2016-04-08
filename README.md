76abc7f2a3abf7093b7837af83923203a58b0297
hasko <hasko@heinecke.com>
2016-04-08 19:17:56 +0000

    Using env var for port

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejstut
+
+Based on the tutorial at http://www.nodebeginner.org/
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var http = require("http");
+const http = require("http");
 
 http.createServer(function(request, response) {
+    console.log("Request received");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World!");
     response.end();
-}).listen(8888);
-
+    console.log("Response finished");
+}).listen(process.env.PORT);
