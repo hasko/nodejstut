8a79a9972a7ae536eebdbb4f13cfe852f0e7322a
hasko <hasko@heinecke.com>
2016-04-10 14:59:10 +0000

    Handling POST data in chunks

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const url = require("url");
 function start(route, handle) {
     
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
-        route(handle, pathname, response);
-    }
+        console.log("Received request for " + pathname);
+        request.setEncoding("utf8");
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk: '" + postDataChunk.slice(0, 10) + "'");
+        });
+        request.addListener("end", function() {
+            console.log("'end' event received");
+            route(handle, pathname, response, postData);
+        })
+     }
     
     http.createServer(onRequest).listen(process.env.PORT);
     console.log("Server started on port " + process.env.PORT);
